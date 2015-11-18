805c75f048342952fda5c99835e2c1f26b95c23d
Oliver Forral <intrepion@gmail.com>
2015-11-18 04:42:48 -0800

    moved code into the component

--- tests/integration/components/a-car-test.js
+++ tests/integration/components/a-car-test.js
@@ -1,25 +1,21 @@
 import { moduleForComponent, test } from 'ember-qunit';
+import setupMirage from '../../helpers/mirage-integration';
 import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('a-car', 'Integration | Component | a car', {
-  integration: true
+  integration: true,
+  setup() {
+    setupMirage(this.container);
+  }
 });
 
 test('it renders', function(assert) {
-  
+
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });" + EOL + EOL +
 
-  this.render(hbs`{{a-car}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:" + EOL +
-  this.render(hbs`
-    {{#a-car}}
-      template block text
-    {{/a-car}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  const car = server.create('car');
+  this.set('car', car);
+  this.render(hbs`{{a-car car=car}}`);
+  assert.equal(this.$().text().trim(), 'Car 1');
 });
